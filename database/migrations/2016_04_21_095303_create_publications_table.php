35a24d1b346de203886b3c47c26cd0662401defe
AndreiRosca <speculum@mail.com>
2016-04-21 13:01:43 +0300

    Added the publications migration table

---
+++ database/migrations/2016_04_21_095303_create_publications_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePublicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('publications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->date('publication_date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('publications');
+    }
+}
