2b7cf5eb840c68be5798462bb2da65237e9157e3
AndreiRosca <speculum@mail.com>
2016-04-21 22:02:56 +0300

    Modified Article model, added the functionality of inserting a new article into the database.

--- app/Article.php
+++ app/Article.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 
 class Article extends BaseModel
 {
@@ -15,5 +18,34 @@ class Article extends BaseModel
     
     public function createArticle($articleParameters) {
         Article::create($articleParameters);
+    }
+    
+    public function getFile($fileName) {
+        return Storage::disk('local')->get($fileName);
+    }
+
+    public function handleArticleFileUpload() {
+        $rules = array('file' => 'required|mimes:doc,docx,pdf');
+        $validator = Validator::make(Input::all(), $rules);
+        if($validator->fails()) {
+            return false;
+        }
+        else {
+            if(Input::hasFile('file')) {
+                $f = Input::file('file');
+                $fileName = date('Y-m-d') . '-' . $f->getClientOriginalName();
+                Storage::disk('local')->put($fileName, file_get_contents($f->getRealPath()));
+                $this->insertArticle($fileName, $f->getMimeType());
+                return true;
+            }
+        }
+    }
+
+    private function insertArticle($fileName, $fileMime) {
+        $this->createArticle(array('english_title' => Input::get('english_title'), 'romanian_title' => Input::get('romanian_title'),
+            'authors' => Input::get('authors'), 'institution' => Input::get('institution'),
+            'publication_id' => Input::get('publication_id'), 'english_description' => Input::get('english_description'),
+            'romanian_description' => Input::get('romanian_description'), 'article_file_name' => $fileName,
+            'article_file_mime' => $fileMime));
     }
 }
