b11457e0cf304ee30dbca0491bd4b5c7b45c700b
PascariIon <jhoonnyc@gmail.com>
2016-04-21 14:29:12 +0300

    added some helpfull templates, header, footer, copyright

---
+++ resources/views/layout/copyright.blade.php
@@ -0,0 +1,35 @@
+<div class="copy-right">
+    <div class="container">
+        <ul>
+            <li>
+                <a href="https://www.facebook.com/pages/Centrul-Mamei-%C8%99i-al-Copilului/152477004840913?fref=ts" target="_blank"></a>
+            </li>
+        </ul>
+        <div class="clearfix"> </div>
+        <p>© 2016 Asociația Chirurgilor Pediatrici din Republica Moldova.</p>
+    </div>
+</div>
+<!--smooth-scrolling-of-move-up-->
+<script type="text/javascript">
+    $(document).ready(function () {
+        /*
+         var defaults = {
+         containerID: 'toTop', // fading element id
+         containerHoverID: 'toTopHover', // fading element hover id
+         scrollSpeed: 1200,
+         easingType: 'linear'
+         };
+         */
+
+        $().UItoTop({
+            easingType: 'easeOutQuart'
+        });
+
+    });
+</script>
+<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+<!--//smooth-scrolling-of-move-up-->
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="js/bootstrap.js"></script>
---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,45 @@
+<!--footer-->
+<div class="footer">
+    <div class="container">
+        <h3 class="title">GET IN <span>TOUCH</span></h3>
+        <div class="footer-grids">
+            <div class="col-md-6 footer-left">
+                <h5><a href="#">Ready to hire me?</a></h5>
+                <div class="more">
+                    <a href="contact.html" class="effect6">Get in touch</a>
+                </div>
+            </div>
+            <div class="col-md-6 footer-right">
+                <div class="address">
+                    <div class="col-xs-2 contact-grdl">
+                        <span class="glyphicon glyphicon-phone" aria-hidden="true"></span>
+                    </div>
+                    <div class="col-xs-10 contact-grdr">
+                        <p>+111 222 3333 111</p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="address">
+                    <div class="col-xs-2 contact-grdl">
+                        <span class="glyphicon glyphicon-send" aria-hidden="true"></span>
+                    </div>
+                    <div class="col-xs-10 contact-grdr">
+                        <p>345 Diamond Street,London.</p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+                <div class="address">
+                    <div class="col-xs-2 contact-grdl">
+                        <span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>
+                    </div>
+                    <div class="col-xs-10 contact-grdr">
+                        <p><a href="mailto:example@mail.com">mail@example.com</a></p>
+                    </div>
+                    <div class="clearfix"> </div>
+                </div>
+            </div>
+            <div class="clearfix"> </div>
+        </div>
+    </div>
+</div>
+<!--//footer-->
