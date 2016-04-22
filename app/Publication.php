59e9be0e4bf0f5a4d1e3d13d407fd580151ea092
AndreiRosca <speculum@mail.com>
2016-04-22 12:06:06 +0300

    Modified Publication entity, added the possibility to view the list of articles from a publication

--- app/Publication.php
+++ app/Publication.php
@@ -43,4 +43,29 @@ class Publication extends BaseModel
         }
         return $publicationList;
     }
+
+    public function getPublication($id) {
+        /*$articles = DB::table('publications')->join('articles', 'publications.id', '=', 'articles.publication_id')->
+        select('publication.id as publication_id', 'title', 'publication_date', 'english_title', 'romanian_title',
+            'authors', 'institution', 'english_description', 'romanian_description', 'article_file_name',
+            'article_file_mime')->where('publications.id', '=', $id)->get();
+        */
+        $pub = DB::table('publications')->select('title', 'publication_date')->where('id', '=', $id)->get();
+        foreach ($pub as $p) {
+            $publication = array('title' => $p->title, 'publication_date' => $p->publication_date,
+                'articleList' => array());
+        }
+        $articles = DB::table('articles')->
+        select('english_title', 'romanian_title',
+            'authors', 'institution', 'english_description', 'romanian_description', 'article_file_name',
+            'article_file_mime')->where('publication_id', '=', $id)->get();
+        $articleList = array();
+        foreach ($articles as $a) {
+            $articleList[] = array('english_title' => $a->english_title, 'romanian_title' => $a->romanian_title,
+                'authors' => $a->authors, 'institution' => $a->institution, 'english_description' => $a->english_description,
+                'romanian_description' => $a->romanian_description, 'article_file_name' => $a->article_file_name);
+        }
+        $publication['articleList'] = $articleList;
+        return $publication;
+    }
 }
