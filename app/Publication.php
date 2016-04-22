ba5870dc257c3ed2e898e7b2311d2a671b19a563
AndreiRosca <speculum@mail.com>
2016-04-22 11:24:59 +0300

    Modified Publication entity, added the possibility to retrieve the publication archive

--- app/Publication.php
+++ app/Publication.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
 
 class Publication extends BaseModel
 {
@@ -13,5 +14,33 @@ class Publication extends BaseModel
 
     public function createPublication($title, $publicationDate) {
         Publication::create(array('title' => $title, 'publication_date' => $publicationDate));
+    }
+
+    public function selectPublicationArchive() {
+        /*
+        $publications = DB::table('publications')->
+        select('id', 'title', 'publication_date')->orderBy('publication_date', 'desc')
+            ->orderBy('title', 'asc')->get();
+        $publicationList = array();
+        foreach ($publications as $p) {
+            $publicationList[] = array('id' => $p->id, 'title' => $p->title, 'publication_date' => $p->publication_date);
+        }
+        return $publicationList;
+        */
+
+        $publications = DB::table('publications')->
+        select('id', 'publication_date')->orderBy('publication_date', 'desc')->groupBy('publication_date')->get();
+        $publicationList = array();
+        foreach ($publications as $p) {
+            $publicationTitles = DB::table('publications')->select('id', 'title')->orderBy('title', 'asc')
+                ->where('publication_date', '=', $p->publication_date)->get();
+            $titleList = array();
+            foreach ($publicationTitles as $title) {
+                $titleList[] = array('id' => $title->id, 'title' => $title->title);
+            }
+            $publicationList[] = array('id' => $p->id, 'publication_date' => $p->publication_date,
+                'titleList' => $titleList);
+        }
+        return $publicationList;
     }
 }
