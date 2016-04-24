7c58c1b94c29f26583eb30d4c6accc6e832fbd38
AndreiRosca <speculum@mail.com>
2016-04-24 12:03:57 +0300

    Now the index page really shows the latest 5 articles

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,8 +10,7 @@ use Illuminate\Support\Facades\Response;
 
 class ArticleController extends Controller
 {
-    public function getArticles()
-    {
+    public function getArticles() {
         return View::make("view-publication")->with("articleList", array(array("romanian_title"=> "Alabama sucking bitches","authors"=>"George Carling", "institution"=>"White House", "romanian_description"=>"Have you ever noticed that anybody driving slower than you is an idiot, and anyone going faster than you is a maniac?
 I have as much authority as the Pope, I just don't have as many people who believe it.
 The main reason Santa is so jolly is because he knows where all the bad girls live.", "article_file_name"=>"jirosa.pdf"),array("romanian_title"=> "Alabama sucking bitches","authors"=>"George Carling", "institution"=>"White House", "romanian_description"=>"Have you ever noticed that anybody driving slower than you is an idiot, and anyone going faster than you is a maniac?
--- app/Http/Controllers/LatestArticleController.php
+++ app/Http/Controllers/LatestArticleController.php
@@ -11,13 +11,8 @@ class LatestArticleController extends Controller
 {
     function getLatestArticles(){
         $publication = new \App\Publication();
-        return View::make("index", ["articleList" => array(array("romanian_title" => "Alabama sucking bitches", "authors" => "George Carling", "institution" => "White House", "romanian_description" => "Have you ever noticed that anybody driving slower than you is an idiot, and anyone going faster than you is a maniac?
-I have as much authority as the Pope, I just don't have as many people who believe it.
-The main reason Santa is so jolly is because he knows where all the bad girls live.", "article_file_name" => "jirosa.pdf"), array("romanian_title" => "Alabama sucking bitches", "authors" => "George Carling", "institution" => "White House", "romanian_description" => "Have you ever noticed that anybody driving slower than you is an idiot, and anyone going faster than you is a maniac?
-I have as much authority as the Pope, I just don't have as many people who believe it.
-The main reason Santa is so jolly is because he knows where all the bad girls live.", "article_file_name" => "jirosa.pdf")),
+        $article = new \App\Article();
+        return View::make("index", ["articleList" => $article->getLatestArticles(5),
             "publications" => $publication->selectPublicationArchive()]);
-            //"publications" => array(array("publication_date" => "2015", "id" => 1, "titleList" => array(array("id" => 1, "title" => "Numarul 1"), array("id" => 2, "title" => "Numarul 2"))), array("publication_date" => "2015", "id" => 1, "titleList" => array(array("id" => 1, "title" => "Numarul 1"), array("id" => 2, "title" => "Numarul 2"))))]);
-
     }
 }
