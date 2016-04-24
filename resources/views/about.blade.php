64208fdd7fe12e2a8d1421eb91a6d0ed38c7f00f
PascariIon <jhoonnyc@gmail.com>
2016-04-24 14:38:17 +0300

    Added contributors to about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,8 @@
         <!-- Collect the nav links, forms, and other content for toggling -->
 <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
     <ul class="nav navbar-nav navbar-left">
-        <li><a href="{{url('index')}}" class="link link--yaku"><span>A</span><span>C</span><span>A</span><span>S</span><span>Ă</span></a></li>
+        <li><a href="{{url('index')}}"
+               class="link link--yaku"><span>A</span><span>C</span><span>A</span><span>S</span><span>Ă</span></a></li>
         <li><a href="{{url('search')}}"
                class="link link--yaku"><span>C</span><span>Ă</span><span>U</span><span>T</span><span>A</span><span>R</span><span>E</span></a>
         </li>
@@ -10,8 +11,8 @@
                class="link link--yaku"><span>L</span><span>I</span><span>S</span><span>T</span><span>A</span>
                 <span>L</span><span>U</span><span>C</span><span>R</span><span>Ă</span><span>R</span><span>I</span></a>
         </li>
-        <li  class="active"><a href="{{url('about')}}"
-               ><span>D</span><span>E</span><span>S</span><span>P</span><span>R</span><span>E</span></a>
+        <li class="active"><a href="{{url('about')}}"
+            ><span>D</span><span>E</span><span>S</span><span>P</span><span>R</span><span>E</span></a>
         </li>
         <!-- <li><a href="#" class="dropdown-toggle link link--yaku" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span>G</span><span>A</span><span>L</span><span>E</span><span>R</span><span>I</span><span>I</span><span class="caret"></span></a>
              <ul class="dropdown-menu">
@@ -38,8 +39,12 @@
         <div class="about-info">
             <div class="col-md-12 about-grids grid-top">
                 <h4></h4>
-                <p class="top">Publicaţia Periodică «Arta Medica» (înregistrată la Ministerul Justiţiei al Republicii Moldova sub Nr 123) este orientată spre publicarea editorialelor, lucrărilor originale, referatelor generale, studiilor de cazuri clinice, recenziilor de cărţi şi reviste, referatelor din literatura de specialitate, corespondenţelor, informaţiilor medicale şi se desfăşoară după principiul autofinanţării şi recuperării.</p>
-                <p>Revista on-line este fondată de către Publicaţia Periodică «Arta Medica» şi actualmente apare cu suportul Întreprinderii de Stat MoldData</p>
+                <p class="top">Publicaţia Periodică «ACP» este orientată spre publicarea editorialelor, lucrărilor
+                    originale, referatelor generale, studiilor de cazuri clinice, recenziilor de cărţi şi reviste,
+                    referatelor din literatura de specialitate, corespondenţelor, informaţiilor medicale şi se
+                    desfăşoară după principiul autofinanţării şi recuperării.</p>
+                <p>Revista on-line este fondată de către Publicaţia Periodică «ACP» şi actualmente apare cu suportul lui
+                    Andrei și Ion</p>
 
                 <table width="70%" border="0" align="center">
                     <tr>
@@ -51,13 +56,15 @@
                                 <strong>Alexandru FERDOHLEB</strong><br>
                                 Secretar de redacţie:<br>
                                 <strong>Eduard BERNAZ</strong><br>
-                                Redactor versiune on-line:<br>
-                                <strong>Igor ŞTEFANEŢ</strong></p></td>
+                                Redactor modul logic:<br>
+                                <strong>Andrei ROȘCA</strong><br>
+                                Redactor modul view-controller:<br>
+                                <strong>Ion PASCARI</strong></p></td>
                     </tr>
 
                 </table>
             </div>
-            <div class="clearfix"> </div>
+            <div class="clearfix"></div>
         </div>
     </div>
 </div>
